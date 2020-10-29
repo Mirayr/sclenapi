4cd25ea9bb3525e68288b4e8def2bbe388be787c
mirayr <mirayr.raul@gmail.com>
2020-10-29 17:06:09 -0300

    Fix Mappin in service model
    Algumas anotações da classe servico estavam erradas. Foi mudado de RequestMapping para GetMapping e PostMapping Respectivamente.

--- src/main/java/com/mirayr/br/scleanapi/controller/ServicoResource.java
+++ src/main/java/com/mirayr/br/scleanapi/controller/ServicoResource.java
@@ -8,7 +8,9 @@ import com.mirayr.br.scleanapi.repository.ServicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,24 +26,24 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/api")
 @Api(value="API REST de Servicos")
-@CrossOrigin(value = "*")
+// @CrossOrigin(value = "*")
 public class ServicoResource {
     @Autowired
     ServicoRepository servicoRepository;
 
-    @RequestMapping("/servicos")
+    @GetMapping("/servicos")
     @ApiOperation(value="Retorna uma lista de Serviços")
     public List<Servico> listaServicos(){
         return servicoRepository.findAll();
     }
 
-    @RequestMapping("/servico/{id}")
+    @GetMapping("/servico/{id}")
     @ApiOperation(value="Retorna um unico Serviço")
     public Servico listaServicoUnico(@PathVariable(value="id") long id) {
         return servicoRepository.findById(id);
     }
 
-    @RequestMapping("/servico")
+    @PostMapping("/servico")
     @ApiOperation(value="Salva um Serviço novo")
     public Servico salvaServico(@RequestBody Servico servico) {
         return servicoRepository.save(servico);
