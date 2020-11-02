f57d8c5de52290d64135051212dfcdad8407f144
mirayr <mirayr.raul@gmail.com>
2020-11-01 23:45:24 -0300

    Its is a model Pedido.
    This model have atributes and method of it. This model have a new atribute Date with TemporalType

---
+++ src/main/java/com/mirayr/br/scleanapi/models/Pedido.java
@@ -0,0 +1,62 @@
+package com.mirayr.br.scleanapi.models;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TemporalType;
+
+// import org.springframework.data.jpa.repository.Temporal;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Pedido")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "data_pedido")
+    @javax.persistence.Temporal(TemporalType.DATE)
+    private Date dataPedido;    
+    
+    
+
+    
+    private java.util.Date parseDate(String date) {
+        final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return new Date(DATE_FORMAT.parse(date).getTime());
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public void set_dataPedido(String date){
+        this.dataPedido = parseDate(date);
+    }
+
+    public Date get_dataPedido(){
+        return this.dataPedido;
+    }
+
+    public Pedido(String data){
+        this.dataPedido = parseDate(data);
+    }
+
+}
