873e2300ab5403d26c31f4ed64f34d10e03b1494
mirayr <mirayr.raul@gmail.com>
2020-11-01 23:53:49 -0300

    Create a new Repository from Pedido

---
+++ src/main/java/com/mirayr/br/scleanapi/repository/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.mirayr.br.scleanapi.repository;
+
+import com.mirayr.br.scleanapi.models.Pedido;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface PedidoRepository extends JpaRepository <Pedido, Long>{
+    
+}
