11d74f1d2fee2023fffe996878c2b39a0d9c4723
mirayr <mirayr.raul@gmail.com>
2020-11-01 23:54:25 -0300

    Nothin try use Repository Anotation

--- src/main/java/com/mirayr/br/scleanapi/repository/ClienteRepository.java
+++ src/main/java/com/mirayr/br/scleanapi/repository/ClienteRepository.java
@@ -4,6 +4,8 @@ import com.mirayr.br.scleanapi.models.Cliente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
     Cliente findById(long id);
 }
