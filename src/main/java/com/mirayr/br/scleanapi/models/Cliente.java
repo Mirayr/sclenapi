b7e95230464ac2b42d442168be63ddf63a92c034
mirayr <mirayr.raul@gmail.com>
2020-11-01 23:51:24 -0300

    Add relatioship to Pedido
    Add a new feature to OneToMany with Pedidos table.

--- src/main/java/com/mirayr/br/scleanapi/models/Cliente.java
+++ src/main/java/com/mirayr/br/scleanapi/models/Cliente.java
@@ -1,12 +1,17 @@
 package com.mirayr.br.scleanapi.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -27,4 +32,11 @@ public class Cliente implements Serializable{
 
     @Column
     String  nome, telefone, cpf, cidade, bairro, rua, numero, complemento, email, observacao;
+
+    @OneToMany(
+        cascade = CascadeType.ALL,
+        orphanRemoval = false
+    )
+    @JoinColumn(name = "cliente_id")
+    private List<Pedido> pedidos = new ArrayList <>();
 }
