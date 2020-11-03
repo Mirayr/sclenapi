f1b2b53989bb3aa2565722bd3f8442843670e80d
mirayr <mirayr.raul@gmail.com>
2020-11-03 02:46:24 -0300

    Changes to work and teste OneToOne relatioship

--- src/main/java/com/mirayr/br/scleanapi/ScleanapiApplication.java
+++ src/main/java/com/mirayr/br/scleanapi/ScleanapiApplication.java
@@ -1,8 +1,5 @@
 package com.mirayr.br.scleanapi;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.mirayr.br.scleanapi.models.Cliente;
 import com.mirayr.br.scleanapi.models.Pedido;
 import com.mirayr.br.scleanapi.repository.ClienteRepository;
@@ -26,15 +23,33 @@ public class ScleanapiApplication {
 		
 		PedidoRepository pedidoRepository = configurableApplicationContext.getBean(PedidoRepository.class);
 
-		Cliente cliente = clienteRepository.findById(47);
+		Cliente queila = clienteRepository.findById(47); // Queila
+		Cliente flair2 = clienteRepository.findById(30); // Flair
+ 
 
-		Pedido pedido1 = new Pedido("1999-09-22");
-		Pedido pedido2 = new Pedido("2020-07-18");
-		Pedido pedido3 = new Pedido("2020-03-25");
-		List<Pedido> pedidos = Arrays.asList(pedido1, pedido2, pedido3);
-		System.out.println("Nome do Cliente " + cliente.getNome());
-		cliente.setPedidos(pedidos);
-		clienteRepository.save(cliente);
+		
+		Pedido pedido1 = new Pedido("1999-02-02");
+		Pedido pedido2 = new Pedido("2020-02-02");
+		Pedido pedido3 = new Pedido("2020-02-02");
+		Pedido pedido4 = new Pedido("2020-02-02");
+		// List<Pedido> pedidos = Arrays.asList(pedido1, pedido2, pedido3, pedido4);
+		System.out.println("Nome do Cliente " + flair2.getNome());
+		// flair2.setPedidos(pedidos);
+		flair2.setPedido(pedido1);
+		flair2.setPedido(pedido2);
+		flair2.setPedido(pedido3);
+		flair2.setPedido(pedido4);
+
+		clienteRepository.save(flair2);
+
+		for (Pedido pedido : flair2.getPedidos()){
+			System.out.println("Nome: " + pedido.getCliente().getNome() +  "  Data do Pedido: " + pedido.getDataPedido()+" Id do pedido "+ pedido.getId());
+			if (pedido.getId() == 98){
+				System.out.println("Encontrei o pedido 98");
+				// pedidoRepository.delete(pedido);
+			}
+		}
+		
 	}
 
 }
