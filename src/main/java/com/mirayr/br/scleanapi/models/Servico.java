cfead8f6ce4e0a34630cc1b2653b96d7db4499b3
mirayr <mirayr.raul@gmail.com>
2020-11-07 00:14:09 -0300

    Configura to work with many to many mode

--- src/main/java/com/mirayr/br/scleanapi/models/Servico.java
+++ src/main/java/com/mirayr/br/scleanapi/models/Servico.java
@@ -2,13 +2,21 @@ package com.mirayr.br.scleanapi.models;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.criteria.Fetch;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,4 +38,30 @@ public class Servico implements Serializable{
     private BigDecimal valor_base;
     @Column
     private String descricao;
+
+    @ManyToMany(    fetch = FetchType.LAZY
+                    //,cascade = CascadeType.ALL
+    )
+    @JoinTable(
+        name = "pedido_servico",
+        joinColumns = {@JoinColumn(name = "servico_id")},
+        inverseJoinColumns = {@JoinColumn(name = "pedido_id")}
+    )
+    private Set<Pedido> pedidos = new HashSet<Pedido>();
+
+    public void setPedido(Pedido pedido){
+        this.pedidos.add(pedido);
+    }
+
+    public void removePedido(Pedido pedido){
+        this.pedidos.remove(pedido);
+    }
+
+    public void setPedidos(Set<Pedido> pedidos){
+        this.pedidos = pedidos;
+    }
+
+    public Set<Pedido> getPedidos(){
+        return this.pedidos;
+    }
 }
