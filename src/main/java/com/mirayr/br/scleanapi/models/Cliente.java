a7e20c5da3b26c0ff6d0f92aeb30d5fd68e97f9a
mirayr <mirayr.raul@gmail.com>
2020-11-14 03:19:09 -0300

    Update remove lombock anotattion and using jacosn

--- src/main/java/com/mirayr/br/scleanapi/models/Cliente.java
+++ src/main/java/com/mirayr/br/scleanapi/models/Cliente.java
@@ -14,34 +14,243 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @Entity
 @Table(name = "cliente")
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class Cliente implements Serializable{
-    private static final long serialVersionUID = 1L;
-    
+@JsonIgnoreProperties(value = { "pedidos" }, allowSetters = true)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+public class Cliente implements Serializable {
+    public static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    public long id;
 
     @Column
-    String  nome, telefone, cpf, cidade, bairro, rua, numero, complemento, email, observacao;
+    public String nome;
+    @Column
+    public String telefone;
+    @Column
+    public String cpf;
+    @Column
+    public String cidade;
+    @Column
+    public String bairro;
+    @Column
+    public String rua;
+    @Column
+    public String numero;
+    @Column
+    public String complemento;
+    @Column
+    public String email;
+    @Column
+    public String observacao;
 
-    @OneToMany(        
-        cascade = CascadeType.ALL,
-        orphanRemoval = true
-    )
+    @OneToMany(cascade = CascadeType.REMOVE, orphanRemoval = true)
     @JoinColumn(name = "cliente_id", referencedColumnName = "id")
-    private Set<Pedido> pedidos = new HashSet<Pedido>();
+    @JsonProperty("pedidos")
+    public Set<Pedido> pedidos = new HashSet<Pedido>();
 
-    public void setPedido(Pedido pedido){
+    public void setPedido(Pedido pedido) {
         pedidos.add(pedido);
-        pedido.setCliente(this);                       
+        pedido.setCliente(this);
     }
+
+    /**
+     * @return long return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return String return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return String return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return String return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return String return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return String return the rua
+     */
+    public String getRua() {
+        return rua;
+    }
+
+    /**
+     * @param rua the rua to set
+     */
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    /**
+     * @return String return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return String return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the observacao
+     */
+    public String getObservacao() {
+        return observacao;
+    }
+
+    /**
+     * @param observacao the observacao to set
+     */
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    /**
+     * @return Set<Pedido> return the pedidos
+     */
+    // @JsonManagedReference("cliente")
+    public Set<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    /**
+     * @param pedidos the pedidos to set
+     */
+    public void setPedidos(Set<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public Cliente(long id, String nome, String telefone, String cpf, String cidade, String bairro, String rua,
+            String numero, String complemento, String email, String observacao,
+            Set<com.mirayr.br.scleanapi.models.Pedido> pedidos) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.email = email;
+        this.observacao = observacao;
+        this.pedidos = pedidos;
+    }
+
+    public Cliente() {
+    }
+
 }
